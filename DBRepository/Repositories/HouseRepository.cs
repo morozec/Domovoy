c1f48242abc5cc846f46844c4c275681727f678f
DmitriiProkofev <36125926+DmitriiProkofev@users.noreply.github.com>
2019-06-29 23:59:16 +0400

    REPO house

---
+++ DBRepository/Repositories/HouseRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBRepository.Factories;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DBRepository.Repositories
+{
+    public class HouseRepository : BaseRepository, IHouseRepository
+    {
+        public HouseRepository(string connectionString, IDomovoyContextFactory domovoyContextFactory) : base(
+            connectionString, domovoyContextFactory)
+        {
+        }
+
+        public async Task<List<House>> GetHouses()
+        {
+            using (var context = DomovoyContextFactory.CreateDbContext(ConnectionString))
+            {
+                return await context.Houses.ToListAsync();
+            }
+        }
+
+        public async Task<List<House>> GetHousesByAddress(string address)
+        {
+            using (var context = DomovoyContextFactory.CreateDbContext(ConnectionString))
+            {
+                return await context.Houses.Where(h => h.Address.Contains(address)).ToListAsync();
+            }
+        }
+
+        public async Task<House> GetHousesById(int id)
+        {
+            using (var context = DomovoyContextFactory.CreateDbContext(ConnectionString))
+            {
+                return await context.Houses.SingleOrDefaultAsync(h => h.HouseId == id);
+            }
+        }
+    }
+}
---
+++ DBRepository/Repositories/IHouseRepository.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBRepository.Repositories
+{
+    public interface IHouseRepository
+    {
+        Task<List<House>> GetHouses();
+
+        Task<List<House>> GetHousesByAddress(string address);
+
+
+    }
+}
