7b0436c2334f639cbf16aefa3e57d53a6ef9ca1e
DmitriiProkofev <36125926+DmitriiProkofev@users.noreply.github.com>
2019-06-29 14:42:56 +0400

    Добавил инициализацию бд.

---
+++ Domovoy/Data/DomovoyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Domovoy.Models.DomainObjects;
+
+namespace Domovoy.Data
+{
+    public class DomovoyContext : IdentityDbContext<User, IdentityRole<long>, long>
+    {
+        public DomovoyContext(DbContextOptions<DomovoyContext> options) : base(options)
+        {
+        }
+
+    }
+}
---
+++ Domovoy/Models/DomainObjects/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domovoy.Models.DomainObjects
+{
+    /// <summary>
+    /// Пользователь.
+    /// </summary>
+    public class User : IdentityUser<long>
+    {
+        #region Constructors
+        #endregion //Constructors
+
+        #region DataProperties
+
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия.
+        /// </summary>
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+
+        #endregion //DataProperties
+    }
+}
