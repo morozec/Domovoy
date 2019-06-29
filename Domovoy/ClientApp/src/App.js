a0974ac7cbbc809afa152b3b562b218ce56b884a
Сергей Шадров <grayhb@mail.ru>
2019-06-30 00:15:01 +0400

    Добавил компонент для выбранного дома

--- Domovoy/ClientApp/src/App.js
+++ Domovoy/ClientApp/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { MapComponent } from './components/MapComponent';
-
+import { HouseComponent } from './components/House/HouseComponent';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -10,7 +10,8 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={MapComponent} />       
+            <Route exact path='/' component={MapComponent} />
+            <Route exact path='/House' component={HouseComponent} />  
       </Layout>
     );
   }
---
+++ Domovoy/ClientApp/src/components/House/HouseComponent.js
@@ -0,0 +1,20 @@
+﻿import React from 'react'
+
+export class HouseComponent extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div id='component-root'>
+               <h1>13123123</h1>
+            </div>
+        )
+    }
+}
