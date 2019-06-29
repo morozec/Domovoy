c6b71abc7740edfc67e08e9e1c191c6ce43bdcdb
morozec <andre15@inbox.ru>
2019-06-29 16:32:09 +0400

    UserRepository

---
+++ DBRepository/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+
+namespace Domovoy.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetUsers();
+    }
+}
---
+++ DBRepository/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domovoy.Data.Factories;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Domovoy.Data.Repositories
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        public UserRepository(string connectionString, IDomovoyContextFactory domovoyContextFactory) : base(
+            connectionString, domovoyContextFactory)
+        {
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            using (var context = DomovoyContextFactory.CreateDbContext(ConnectionString))
+            {
+                return await context.Users.ToListAsync();
+            }
+        }
+
+        
+    }
+}
