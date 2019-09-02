4c05d591b499f9c775ff5e7cf789e15877f14ccb
MorozovAP <andre15@inbox.ru>
2019-09-02 11:23:18 +0400

    PrivateRoute to Profile

--- Domovoy/ClientApp/src/components/Layout.js
+++ Domovoy/ClientApp/src/components/Layout.js
@@ -1,100 +1,92 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from 'reactstrap';
-import  NavMenu  from './NavMenu';
+import NavMenu from './NavMenu';
 import { MapComponent } from './MapComponent'
 import { HouseComponent } from './House/HouseComponent'
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { Navbar } from 'react-bootstrap'
 import Profile from './Profile'
+import PrivateRoute from './PrivateRoute';
 
-export class Layout extends Component {
-  static displayName = Layout.name;
-  constructor() {
-    super()
-    this.state = {
-      isSearched: true,
-      house: undefined,
-      houses: undefined,
-      map:undefined
-    }
+const Layout = () => {
+  const [isSearched, setIsSearched] = useState(true)
+  const [house, setHouse] = useState(undefined)
+  const [houses, setHouses] = useState(undefined)
+  const [map, setMap] = useState(undefined)
 
-    this.updateHouse = this.updateHouse.bind(this)
-    this.clearHouse = this.clearHouse.bind(this)
-    this.updateMap = this.updateMap.bind(this)
-  }
-
-  updateHouse(houseId, isSearched) {
+  const updateHouse = (houseId, isSearched) => {
     fetch(`api/GeoData/GetHouse/${houseId}`)
       .then(response => response.json())
       .then(data => {
-        this.setState({ isSearched: isSearched, house: data })
-      });
+        setIsSearched(isSearched)
+        setHouse(data)
+      })
   }
 
-  clearHouse() {
-    this.setState({ isSearched: false, house: undefined })
+  const clearHouse = () => {
+    setIsSearched(false)
+    setHouse(undefined)
   }
 
-  updateMap(map){
-    //console.log('layout', mapExtent)
-    this.setState({map:map})
-  }
+  const updateMap = (map) => setMap(map)
 
-  componentDidMount() {
+
+  useEffect(() => {
+    console.log('Layout mount')
     fetch(`api/GeoData/GetHouses`)
       .then(response => response.json())
       .then(houses => {
-        this.setState({houses:houses})
+        setHouses(houses)
       })
+  }, [])
 
-  }
 
-
-  render() {
-    return (
-      <div>
-        <NavMenu handleMenuSelected={this.updateHouse} />
-        <Container>
+  return (
+    <div>
+      <NavMenu handleMenuSelected={this.updateHouse} />
+      <Container>
+        <Switch>
           <Route exact path='/' render={(props) => (
             <MapComponent {...props}
-              house={this.state.house}
-              updateHouse={this.updateHouse}
-              isSearched={this.state.isSearched}
-              clearHouse={this.clearHouse}
-              houses={this.state.houses}
-              map = {this.state.map}
-              updateMap = {this.updateMap}
+              house={house}
+              updateHouse={updateHouse}
+              isSearched={isSearched}
+              clearHouse={clearHouse}
+              houses={houses}
+              map={map}
+              updateMap={updateMap}
             />
           )} />
           <Route exact path='/House/:id' component={HouseComponent} />
           <Route exact path='/img/test' component={HouseComponent} />
-          <Route exact path='/profile' component={Profile} />
+          <PrivateRoute exact path='/profile' component={Profile} />
+        </Switch>
+      </Container>
+
+
+      <Navbar sticky="bottom" className="navbar navbar-expand-md navbar-dark bg-dark">
+        <Container>
+          <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="http://eias.fstrf.ru/jkh_calc/#63" target="_blank">КАЛЬКУЛЯТОР КОММУНАЛЬНЫХ ПЛАТЕЖЕЙ</a>
+              </li>
+            </ul>
+          </div>
+
+
+          <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#">2019</a>
+              </li>
+            </ul>
+          </div>
         </Container>
+      </Navbar>
 
-
-        <Navbar sticky="bottom" className="navbar navbar-expand-md navbar-dark bg-dark">
-          <Container>
-            <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="http://eias.fstrf.ru/jkh_calc/#63" target="_blank">КАЛЬКУЛЯТОР КОММУНАЛЬНЫХ ПЛАТЕЖЕЙ</a>
-                </li>
-
-              </ul>
-            </div>
-
-
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="#">2019</a>
-                </li>
-              </ul>
-            </div>
-          </Container>
-        </Navbar>
-
-      </div>
-    );
-  }
+    </div>
+  )
 }
+
+export { Layout }
