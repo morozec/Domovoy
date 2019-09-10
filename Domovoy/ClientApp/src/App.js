6e5239377d29ca61e68b8f47b07b7f8b0f8fba86
MorozovAP <andre15@inbox.ru>
2019-09-10 09:16:05 +0400

    контент - на всю доступную высоту

--- Domovoy/ClientApp/src/App.js
+++ Domovoy/ClientApp/src/App.js
@@ -1,18 +1,99 @@
 import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import { NavMenu } from './components/NavMenu';
+import { MapComponent } from './components/MapComponent'
+import { HouseComponent } from './components/House/HouseComponent'
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { MapComponent } from './components/MapComponent';
-import { HouseComponent } from './components/House/HouseComponent';
+import { Navbar } from 'react-bootstrap'
 import './components/Main.css';
 
 export default class App extends Component {
-  static displayName = App.name;
+  static displayName = App.name; 
+  constructor() {
+    super()
+    this.state = {
+      isSearched: true,
+      house: undefined,
+      houses: undefined,
+      map:undefined
+    }
+
+    this.updateHouse = this.updateHouse.bind(this)
+    this.clearHouse = this.clearHouse.bind(this)
+    this.updateMap = this.updateMap.bind(this)
+  }
+
+  updateHouse(houseId, isSearched) {
+    fetch(`api/GeoData/GetHouse/${houseId}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ isSearched: isSearched, house: data })
+      });
+  }
+
+  clearHouse() {
+    this.setState({ isSearched: false, house: undefined })
+  }
+
+  updateMap(map){
+    //console.log('layout', mapExtent)
+    this.setState({map:map})
+  }
+
+  componentDidMount() {
+    fetch(`api/GeoData/GetHouses`)
+      .then(response => response.json())
+      .then(houses => {
+        this.setState({houses:houses})
+      })
+
+  }
 
   render () {
     return (
-      <Layout>
-            
-      </Layout>
+      <div className='layout'>
+      <NavMenu handleMenuSelected={this.updateHouse} />
+      <Container className='layout-content'>
+        <Route exact path='/' render={(props) => (
+          <MapComponent {...props}
+            house={this.state.house}
+            updateHouse={this.updateHouse}
+            isSearched={this.state.isSearched}
+            clearHouse={this.clearHouse}
+            houses={this.state.houses}
+            map = {this.state.map}
+            updateMap = {this.updateMap}
+          />
+        )} />
+        <Route exact path='/House/:id' component={HouseComponent} />
+
+        <Route exact path='/img/test' component={HouseComponent} />
+      </Container>
+
+
+      <Navbar sticky="bottom" className="navbar navbar-expand-md navbar-dark bg-dark">
+        <Container>
+          <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="http://eias.fstrf.ru/jkh_calc/#63" target="_blank">КАЛЬКУЛЯТОР КОММУНАЛЬНЫХ ПЛАТЕЖЕЙ</a>
+              </li>
+
+            </ul>
+          </div>
+
+
+          <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#">2019</a>
+              </li>
+            </ul>
+          </div>
+        </Container>
+      </Navbar>
+
+    </div>
     );
   }
 }
