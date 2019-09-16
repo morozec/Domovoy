4da1098522b762fd6e5705f5e420561f95b29cda
MorozovAP <andre15@inbox.ru>
2019-09-16 15:42:58 +0400

    рефакторинг логина

--- Domovoy/ClientApp/src/components/Login/ValidationLoginForm.js
+++ Domovoy/ClientApp/src/components/Login/ValidationLoginForm.js
@@ -5,123 +5,119 @@ import * as Yup from 'yup'
 import { Form, Label, Input, FormGroup, Button } from 'reactstrap'
 import './ValidationLoginForm.css'
 
-const ValidationLoginForm = () => (
-    <Formik
-        initialValues={{ email: "", password: "" }}        
-        onSubmit={(values, { setSubmitting }) => {
-            // setTimeout(() => {
-            //     console.log('logging in', values)
-            //     setSubmitting(false)
-            // }, 500)
-            let data = {
-                userName:values.email,
-                password:values.password
-            }            
-            
-            fetch('api/Identity/token', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json; charset=utf-8'
-                },
-                body: JSON.stringify(data)
-            }).then(response => {
-                setSubmitting(false)
-                if (response.ok) {                
-                    let rJson = response.json()    
-                    console.log(rJson)
-                    return rJson
-                }else{
-                    throw 'Ошибка авторизации'
-                }
-            }).then(data => {
-                console.log(data)
-            }).catch(ex => {
-                alert(ex)
-            })
+const ValidationLoginForm = () => {
+    const handleSubmit = (values, setSubmitting) => {
+        let data = {
+            userName: values.email,
+            password: values.password
         }
-        }
-        //********Handling validation messages yourself*******/
-        // validate={values => {
-        //     let errors = {}
-        //     if (!values.email) {
-        //         errors.email = "Обзательное поле"
-        //     } else if (!EmailValidator.validate(values.email)) {
-        //         errors.email = "Некорректный email"
-        //     }
 
-        //     const passwordRegex = /(?=.*[0-9])/;
+        fetch('api/Identity/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        }).then(response => {
+            setSubmitting(false)
+            if (response.ok) {
+                let rJson = response.json()
+                console.log(rJson)
+                return rJson
+            } else {
+                throw 'Ошибка авторизации'
+            }
+        }).then(data => {
+            console.log(data)
+        }).catch(ex => {
+            alert(ex)
+        })
+    }
+    return (
+        <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values, {setSubmitting}) => handleSubmit(values, setSubmitting)}
+            //********Handling validation messages yourself*******/
+            // validate={values => {
+            //     let errors = {}
+            //     if (!values.email) {
+            //         errors.email = "Обзательное поле"
+            //     } else if (!EmailValidator.validate(values.email)) {
+            //         errors.email = "Некорректный email"
+            //     }
 
-        //     if (!values.password) {
-        //         errors.password = "Обзательное поле"
-        //     } else if (values.password.length < 8) {
-        //         errors.password = "Длина пароля должна быть не менее 8 символов"
-        //     } else if (!passwordRegex.test(values.password)) {
-        //         errors.password = "Некорретный пароль. Пароль должен содержать хотя бы одну цифру"
-        //     }
+            //     const passwordRegex = /(?=.*[0-9])/;
 
-        //     return errors
-        // }}
-        validationSchema={Yup.object().shape({
-            email: Yup.string()
-                .email()
-                .required('Обзательное поле'),
-            password: Yup.string()
-                .required('Обзательное поле')
-                .min(8, 'Длина пароля должна быть не менее 8 символов')
-                .matches(/(?=.*[0-9])/, 'Некорретный пароль. Пароль должен содержать хотя бы одну цифру')
-        })}
-    >
-        {props => {          
-            const {
-                values,
-                touched,
-                errors,
-                isSubmitting,
-                handleChange,
-                handleBlur,
-                handleSubmit
-            } = props;
-            return (
-                <Form onSubmit={handleSubmit}>
-                    <FormGroup>
-                        <Label for='email'>Email</Label>
-                        <Input
-                            id='email'
-                            placeholder='Введите адрес электронной почты'
-                            value={values.email}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            className={errors.email && touched.email && 'error'}
-                        />
-                        {errors.email && touched.email && (
-                            <div className='input-feedback'>{errors.email}</div>
-                        )}
-                    </FormGroup>
+            //     if (!values.password) {
+            //         errors.password = "Обзательное поле"
+            //     } else if (values.password.length < 8) {
+            //         errors.password = "Длина пароля должна быть не менее 8 символов"
+            //     } else if (!passwordRegex.test(values.password)) {
+            //         errors.password = "Некорретный пароль. Пароль должен содержать хотя бы одну цифру"
+            //     }
 
-                    <FormGroup>
-                        <Label for='password'>Пароль</Label>
-                        <Input
-                            id='password'
-                            type='password'
-                            placeholder='Введите пароль'
-                            value={values.password}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            className={errors.password && touched.password && 'error'}
-                        />
-                        {errors.password && touched.password && (
-                            <div className="input-feedback">{errors.password}</div>
-                        )}
-                    </FormGroup>
+            //     return errors
+            // }}
+            validationSchema={Yup.object().shape({
+                email: Yup.string()
+                    .email()
+                    .required('Обзательное поле'),
+                password: Yup.string()
+                    .required('Обзательное поле')
+                    .min(8, 'Длина пароля должна быть не менее 8 символов')
+                    .matches(/(?=.*[0-9])/, 'Некорретный пароль. Пароль должен содержать хотя бы одну цифру')
+            })}
+        >
+            {props => {
+                const {
+                    values,
+                    touched,
+                    errors,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit
+                } = props;
+                return (
+                    <Form onSubmit={handleSubmit}>
+                        <FormGroup>
+                            <Label for='email'>Email</Label>
+                            <Input
+                                id='email'
+                                placeholder='Введите адрес электронной почты'
+                                value={values.email}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className={errors.email && touched.email && 'error'}
+                            />
+                            {errors.email && touched.email && (
+                                <div className='input-feedback'>{errors.email}</div>
+                            )}
+                        </FormGroup>
 
-                    <Button type='submit' disabled={isSubmitting}>Войти</Button>
-                </Form>
-            )
-        }}
+                        <FormGroup>
+                            <Label for='password'>Пароль</Label>
+                            <Input
+                                id='password'
+                                type='password'
+                                placeholder='Введите пароль'
+                                value={values.password}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className={errors.password && touched.password && 'error'}
+                            />
+                            {errors.password && touched.password && (
+                                <div className="input-feedback">{errors.password}</div>
+                            )}
+                        </FormGroup>
 
-    </Formik>
+                        <Button type='submit' disabled={isSubmitting}>Войти</Button>
+                    </Form>
+                )
+            }}
 
-
-)
+        </Formik>
+    )
+}
 
 export default ValidationLoginForm
